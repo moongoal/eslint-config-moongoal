d595d164e53155605f1248cf06b022adff148b7f
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2023-09-23 11:26:56 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    rules: {
+        quotes: ["error", "double"],
+        "arrow-parens": ["warn", "as-needed"],
+        "comma-dangle": ["warn", "never"],
+        "semi": ["error", "always", { omitLastInOneLineBlock: true }]
+    }
+};
+
+module.exports = config;
